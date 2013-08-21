caa2d660883ec4e8d6aeda9963303554b76fdd2c
Hetty de Vries <hetty82@gmail.com>
2013-08-21 21:57:19 +0200

    Add some stuff. Still didn't fix it. Damn you Rails 4.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,20 @@
 class TasksController < ApplicationController
 
   def index
+    @task = Task.new
     @tasks = Task.all
   end
 
   def create
-    Task.create params[:task]
-    redirect_to :back
+    Task.create (task_params)
+    # redirect_to :back
+    render :text => params.inspect
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 
 end
